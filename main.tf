391a54d691880514a5d482fe7abf1ffd53fedbcf
hadithya <hhadithya34@gmail.com>
2025-06-02 10:43:17 +0530

    feat: Add S3 bucket resource configuration

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 5.0"
     }
   }
   required_version = ">= 1.2.0"
@@ -10,4 +10,16 @@ terraform {
 
 provider "aws" {
   region = "ap-southeast-1"
+}
+
+resource "aws_s3_bucket" "bucket-init-test" {
+  # S3 bucket names must be globally unique across ALL of AWS.
+  bucket =  "terraform-bucket-init-hh-2025-06-02"
+
+  # Add tags, which is a best practice
+  tags = {
+    Name = "TerraformBucket"
+    Project = "TerraformAwsInit"
+    Owner = "hhadithya"
+  }
 }
